8c332a948ada2b1b41d9dbb6f58e8c3fa1b597c7
chachamana <info@manamiw.com>
2024-12-11 14:11:46 +0000

    DAy11 added server setting and external files

--- expense_tracker/server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-// import a middleware to manage Router (Express module)
-const routes=require('express').Router();
-
-
-// when a client sends GET requests to '/api/categories '
-routes.route('/api/categories') //define endpoint(api/categories  URL)
-.get((req,res)=>res.json('Get Request From Categories')) //'return springs
