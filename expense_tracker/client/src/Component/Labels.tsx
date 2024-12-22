ec2a1b07b729220a06aa2a97f30e256a4b2fc800
chachamana <info@manamiw.com>
2024-12-22 22:09:51 +0000

    DAY21 more RTK Query study & fetched label data

--- expense_tracker/client/src/Component/Labels.tsx
+++ expense_tracker/client/src/Component/Labels.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { apiSlice as api} from "../store/apiSlice";
+import { useGetLabelsQuery, useGetCategoriesQuery } from "../store/apiSlice";
 
 type DataType = {
   type: string;
@@ -7,34 +7,23 @@ type DataType = {
   percent: number;
 };
 
-const obj: DataType[] = [
-  {
-    type: "Living Expenses",
-    color: "#f74c71",
-    percent: 45,
-  },
-  {
-    type: "Entertainment & Hobbies",
-    color: "#f9c74f",
-    percent: 20,
-  },
-  {
-    type: "Transportation",
-    color: "#4ff9e5",
-    percent: 10,
-  },
-  {
-    type: "Savings & Investments",
-    color: "#4f63f9",
-    percent: 10,
-  },
-  {
-    type: "Others",
-    color: "#f94f9e",
-    percent: 10,
-  },
-];
+export default function Labels() {
+  // Using the query hook to fetch the data
 
+  const { data, isFetching, isSuccess, isError } = useGetLabelsQuery();
+
+  // Default state
+  let Transactions: React.ReactNode;
+
+  if (isFetching) {
+    Transactions = <div>Fetching</div>;
+  } else if (isSuccess && data) {
+    Transactions = data.map((v: DataType, index: number) => <LabelComponent key={index} data={v} />);
+  } else if (isError) {
+    Transactions = <div>Error</div>;
+  }
+  return <>{Transactions}</>;
+}
 
 type LabelComponentProps = {
   data: DataType;
@@ -45,29 +34,10 @@ function LabelComponent({ data }: LabelComponentProps) {
   return (
     <div className="labels flex justify-between">
       <div className="flex gap-2">
-        <div
-          className="w-2 h-2 rounded py-3"
-          style={{ background: data.color ?? `#f9c74f` }}
-        ></div>
+        <div className="w-2 h-2 rounded py-3" style={{ background: data.color ?? `#f9c74f` }}></div>
         <h3 className="text-md">{data.type ?? ""}</h3>
       </div>
       <h3 className="font-bold">{data.percent ?? 0}%</h3>
     </div>
-  );
-}
-
-export default function Labels() {
-
-  // use returned data
-  //useGetCategoriesQuery();  => fetch data from endpoint & state
-const {data , isFetching, isSuccess, isError}=api.useGetCategoriesQuery();
-console.log(data);
-
-  return (
-    <>
-      {obj.map((v, index) => (
-        <LabelComponent key={index} data={v}></LabelComponent>
-      ))}
-    </>
   );
 }
