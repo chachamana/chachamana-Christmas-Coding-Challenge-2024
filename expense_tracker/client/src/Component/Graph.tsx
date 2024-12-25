34099e80937239b6a853e749ae5201361a41d02f
chachamana <info@manamiw.com>
2024-12-25 16:55:38 +0000

    pass data to graph

--- expense_tracker/client/src/Component/Graph.tsx
+++ expense_tracker/client/src/Component/Graph.tsx
@@ -1,37 +1,34 @@
 import { Doughnut } from 'react-chartjs-2';
 import { Chart,ArcElement} from 'chart.js';
 import Labels from "./Labels";
+import { chart_Data, getTotalSum } from '../helper/helper';
+import { useGetLabelsQuery } from '../store/apiSlice';
 
 Chart.register(ArcElement); // without this line, can't show on the display ,,
 
-const config ={
-  data:{
-    datasets: [{
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4,
-      borderRadius:30,
-      spacing:10
-    }]
-  },
-  options:{
-cutout:100
-  }
-}
-
-
 export default function Graph() {
+
+ const { data, isFetching, isSuccess, isError } = useGetLabelsQuery(undefined);
+
+   // Default state
+   let graphData: React.ReactNode;
+
+   if (isFetching) {
+    graphData = <div>Fetching</div>;
+   } else if (isSuccess && data) {
+   graphData = <Doughnut {...chart_Data(data)}></Doughnut>;
+   } else if (isError) {
+    graphData = <div>Error</div>;
+   }
+
   return (
     <div className="flex justify-content max-w-xs mx-auto">
         <div className="item">
             <div className="chart relative">
-                <Doughnut {...config}></Doughnut>
+              {graphData}
                 <h3 className='mb-4 font-bold title'>Total
-                  <span className='block text-3xl text-emerald-400'>${0}</span>
+                  <span className='block text-3xl text-emerald-400'>£{getTotalSum(data) ?? 0}
+                  </span>
                 </h3>
             </div>
 <div className="flex flex-col py-10 gap-4">
